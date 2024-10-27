ac94d91979b620b04660b5532aa143b122a659e8
Ryan MacKenzie <rienmack@gmail.com>
2024-10-27 16:10:30 -0400

    unused import and linking to another page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Input } from '@/components/ui/input'
-
 import { buttonVariants } from "@/components/ui/button"
-import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 
 export default function Home() {
