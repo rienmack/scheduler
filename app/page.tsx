adf3d810c2182503267007941c67fc79400beea3
Ryan MacKenzie <rienmack@gmail.com>
2024-10-27 16:02:50 -0400

    import shadcn input + button and build minimal homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import Image from "next/image";
-
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <Input placeholder='Enter an event name...' />
+          <Button> Continue </Button>
         </div>
       </main >
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
