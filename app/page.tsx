7c2990529b4bd4087b1f8df6e0d889b4cabdd376
Ryan MacKenzie <rienmack@gmail.com>
2024-10-29 06:56:07 -0400

    be able to switch to calendar component

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,50 @@
 
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { buttonVariants } from "@/components/ui/button"
-import Link from 'next/link'
 import { Calendar } from "@/components/ui/calendar"
 import { useState } from "react";
 
+type ComponentToRender = 'meeting' | 'date';
+
 export default function Home() {
-  const [name, setName] = useState<string>('');
+  const [componentToRender, setComponentToRender] = useState<ComponentToRender>('meeting');
+
+  let component: JSX.Element | null;
+  switch (componentToRender) {
+    case 'meeting':
+      component = <Meeting />
+      break;
+    case 'date':
+      component = <Date />
+      break;
+    default:
+      component = null;
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Input placeholder='Enter a meeting name...' onInput={e => setName(e.currentTarget.value)} />
-          {name.length > 0 &&
-            <Button>
-              Continue
-            </Button>
-          }
+          {component}
+          <Button onClick={() => setComponentToRender('date')}>
+            Continue
+          </Button>
         </div>
       </main >
     </div >
   );
 }
 
-function date() {
+function Meeting() {
+  const [name, setName] = useState<string>('');
+  return (
+    <>
+      <Input placeholder='Enter a meeting name...' onInput={e => setName(e.currentTarget.value)} />
+    </>
+  )
+}
+
+function Date() {
   const [dates, setDates] = useState<Date[]>([]);
   return (
     <>
